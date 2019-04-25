4f514694deb4e2864ed84456e6e1e4ea7f8e69b8
lucyUngaro <lucyu12@gmail.com>
2019-04-25 00:07:38 -0400

    First Commit

---
+++ Assets/Scripts/DotGrid.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level
+{
+
+    public char[] Rows;
+}
+
+public class DotGrid : MonoBehaviour
+{
+    public int Rows = 4;
+    public int Cols = 4;
+   
+    private void Awake()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
